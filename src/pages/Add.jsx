2d98fe5d096c95cf94704e07fd6dcdd94f8fcf9f
Zrour Mostfa <moustazro123@gmail.com>
2024-12-07 20:13:28 +0100

    Add validation

--- src/pages/Add.jsx
+++ src/pages/Add.jsx
@@ -4,46 +4,88 @@ import axios from "axios";
 
 export default function Add() {
   const [inputData, setInputData] = useState({ name: "", email: "" });
+  const [errors, setErrors] = useState({});
   const navigate = useNavigate();
+
+  const validate = () => {
+    let isValid = true;
+
+    if (!inputData.name) {
+      setErrors((prevErrors) => ({ ...prevErrors, name: "Name is required" }));
+      isValid = false;
+    } else if (inputData.name.length < 5) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        name: "Name must be at least 5 characters.",
+      }));
+      isValid = false;
+    } else {
+      setErrors((prevErrors) => ({ ...prevErrors, name: "" }));
+    }
+
+    if (!inputData.email) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        email: "Email is required",
+      }));
+      isValid = false;
+    } else if (!/\S+@\S+\.\S+/.test(inputData.email)) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        email: "Email is not valid",
+      }));
+      isValid = false;
+    } else {
+      setErrors((prevErrors) => ({ ...prevErrors, email: "" }));
+    }
+
+    return isValid;
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios
-      .post("http://localhost:3001/users", inputData)
-      .then((res) => {
-        alert("Data Added Successfully");
-        navigate("/");
-      })
-      .catch((err) => console.log(err));
+
+    if (validate()) {
+      axios
+        .post("http://localhost:3001/users", inputData)
+        .then(() => {
+          alert("Data Added Successfully");
+          navigate("/");
+        })
+        .catch((err) => console.log(err));
+    }
   };
+
   return (
     <div className="d-flex justify-content-center align-items-center">
-      <form className="border rounded p-5 w-50">
-        <div class="mb-3">
+      <form className="border rounded p-5 w-50" onSubmit={handleSubmit}>
+        <div className="mb-3">
           <label className="form-label">User Name</label>
           <input
             type="text"
-            class="form-control"
+            className={`form-control ${errors.name ? "is-invalid" : ""}`}
             onChange={(e) =>
               setInputData({ ...inputData, name: e.target.value })
             }
           />
+          {errors.name && <div className="invalid-feedback">{errors.name}</div>}
         </div>
-        <div class="mb-3">
+
+        <div className="mb-3">
           <label className="form-label">Email address</label>
           <input
             type="email"
-            class="form-control"
+            className={`form-control ${errors.email ? "is-invalid" : ""}`}
             onChange={(e) =>
               setInputData({ ...inputData, email: e.target.value })
             }
           />
+          {errors.email && (
+            <div className="invalid-feedback">{errors.email}</div>
+          )}
         </div>
-        <button
-          type="submit"
-          onClick={handleSubmit}
-          class="btn btn-primary w-100"
-        >
+
+        <button type="submit" className="btn btn-primary w-100">
           Submit
         </button>
       </form>
--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -3,24 +3,61 @@ import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
 export default function Edit() {
-    const {id} = useParams();
-    const navigate = useNavigate();
-    const [user , setUser] = useState({});
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [user, setUser] = useState({});
+  const [errors, setErrors] = useState({});
 
-    useEffect(() =>{
-        axios.get("http://localhost:3001/users/" + id)
-        .then(res => setUser(res.data))
-    },[])
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/users/" + id)
+      .then((res) => setUser(res.data));
+  }, []);
 
-    const handleSubmit = (event) => {
-      event.preventDefault();
-      axios.put("http://localhost:3001/users/" + id , user)
-      .then(res => {
+  const validate = () => {
+    let isValid = true;
+
+    if (!user.name) {
+      setErrors((prevErrors) => ({ ...prevErrors, name: "Name is required" }));
+      isValid = false;
+    } else if (user.name.length < 5) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        name: "Name must be at least 5 characters.",
+      }));
+      isValid = false;
+    } else {
+      setErrors((prevErrors) => ({ ...prevErrors, name: "" }));
+    }
+
+    if (!user.email) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        email: "Email is required",
+      }));
+      isValid = false;
+    } else if (!/\S+@\S+\.\S+/.test(user.email)) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        email: "Email is not valid",
+      }));
+      isValid = false;
+    } else {
+      setErrors((prevErrors) => ({ ...prevErrors, email: "" }));
+    }
+
+    return isValid;
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (validate()) {
+      axios.put("http://localhost:3001/users/" + id, user).then((res) => {
         alert("data Update successfully");
         navigate("/");
-      })
-      
+      });
     }
+  };
   return (
     <div className="d-flex justify-content-center align-items-center">
       <form className="border rounded p-5 w-50">
@@ -28,19 +65,21 @@ export default function Edit() {
           <label className="form-label">User Name</label>
           <input
             type="text"
-            value = {user.name}
-            onChange={(e) => setUser({...user , name : e.target.value})}
-            className="form-control"
+            value={user.name}
+            onChange={(e) => setUser({ ...user, name: e.target.value })}
+            className={`form-control ${errors.name ? "is-invalid" : ""}`}
           />
+          {errors.name && <div className="invalid-feedback">{errors.name}</div>}
         </div>
         <div className="mb-3">
           <label className="form-label">Email address</label>
           <input
             type="email"
-            value = {user.email}
-            onChange={(e) => setUser({...user , email : e.target.value})}
-            className="form-control"
+            value={user.email}
+            onChange={(e) => setUser({ ...user, email: e.target.value })}
+            className={`form-control ${errors.email ? "is-invalid" : ""}`}
           />
+          {errors.email && <div className="invalid-feedback">{errors.email}</div>}
         </div>
         <button
           type="submit"
